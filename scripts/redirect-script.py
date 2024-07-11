4e55bb26d891275bd81df071822598b59e7fc6f1
Zhengfei Wang <zhengfei.wang@microsoft.com>
2024-07-11 19:58:29 +0800

    intercept to AOAI

--- scripts/redirect-script.py
+++ scripts/redirect-script.py
@@ -4,6 +4,15 @@ from mitmproxy import http
 def request(flow: http.HTTPFlow) -> None:
     if flow.request.pretty_url.startswith("http://www.google.com"):
         flow.request.host = "www.baidu.com"
-    if flow.request.pretty_url.startswith("http://www.openai.com"):
+    # if flow.request.pretty_url.startswith("http://www.openai.com"):
+    #     flow.request.host = "localhost"
+    #     flow.request.port = 23333
+    if flow.request.host == "www.openai.com":
         flow.request.host = "localhost"
-        flow.request.port = 5000
+        flow.request.port = 23333
+    # TODO: recognize endpoint in host, and set it somewhere, maybe just in header
+    # TODO: identify real request to remote with some identifier, e.g., header
+    if flow.request.pretty_url.startswith("http://<endpoint>.openai.azure.com/"):
+        flow.request.host = "localhost"
+        flow.request.port = 23333
+        flow.request.headers["endpoint"] = "<endpoint>"
--- src/local_gateway/api/v1/routes.py
+++ src/local_gateway/api/v1/routes.py
@@ -11,3 +11,20 @@ async def completions(
 ):
     resp = {"abc": "def"}
     return JSONResponse(content=resp, status_code=200)
+
+
+@router.post(
+    "/openai/deployments/{deployment_name}/chat/completions"
+)
+async def chat_completions(
+    request_data: dict,
+    request: Request,
+    deployment_name=None,
+    api_path=None,
+):
+    resp = {
+        "api": "mocked",
+        "endpoint": request.headers.get("endpoint"),
+        "deployment_name": deployment_name,
+    }
+    return JSONResponse(content=resp, status_code=200)
